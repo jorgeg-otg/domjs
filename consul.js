4b6a914221e0390a3dfed3640785dc6634a7c348
jorge-otg <websiteotg@gmail.com>
2021-06-04 15:20:21 -0500

    first commit

---
+++ consul.js
@@ -0,0 +1,8 @@
+
+// esta es una breve aplicacion que calcula una multiplicacion  de dos datos una constante|
+
+//  cambios beta
+let beta = 123;
+alfa = 453;
+delta = alfa * beta;
+console.log(delta);
